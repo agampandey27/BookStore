611e709480544f2e576a7fd631995d57d8f860a7
Agam Pandey <agampandey705@gmail.com>
2024-04-25 19:34:55 +0530

    Change in CORS origin

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://book-store-frontend-flame.vercel.app/",
+    origin: "https://book-store-frontend-flame.vercel.app",
     method: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
