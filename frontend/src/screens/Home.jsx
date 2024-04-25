384200c9caadd617bae14e462a2a6e4a1f85f6ab
Agam Pandey <agampandey705@gmail.com>
2024-04-25 19:19:07 +0530

    Second commit

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books`)
+      .get(`https://book-store-alpha-eight.vercel.app/books`)
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
